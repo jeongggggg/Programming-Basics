8bec37eb53a0b36af102dfc14e5debcbf9fbff21
dejeong <ahjeong921@gmail.com>
2024-10-03 22:56:35 +0900

    6주차 사용자 정의 자료형 - 기본자료형

---
+++ programming-basic/week06/01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i=10;
+    char a='a';
+    float f=10.1;
+    double d=11.1;
+    
+    printf("size of i = %d\n",sizeof(i));
+    printf("size of a = %d\n",sizeof(a));
+    printf("size of f = %d\n",sizeof(f));
+    printf("size of d = %d\n",sizeof(d));
+    return 0;
+}
