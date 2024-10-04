8439269ecac4ad1277a2bc16e3588d760f82bf21
dejeong <ahjeong921@gmail.com>
2024-10-04 23:06:26 +0900

    사용자 정의 자료형 - typedef

---
+++ programming-basic/week06/06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+ // typedef
+int main(){
+   typedef int integer;
+   typedef char aaa;
+
+   integer i = 1;
+   aaa grade = 'a';
+
+   printf("i = %d grade = %c \n", i, grade);
+
+    return 0;
+}
