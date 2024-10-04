69f41823d7cf0543c3450aec8fc36ec9832f988f
dejeong <ahjeong921@gmail.com>
2024-10-04 23:08:24 +0900

    해보기 1 풀이

---
+++ programming-basic/week06/07.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+ /*
+    다음 각 변수를 선언하고 출력해보세요.
+    int형 변수 i=10, a=‘a’, float형 변수 f=10.1, double형 변수 d=11.1의 값을 가짐
+ */
+int main(){
+    int i = 10;
+    char a = 'a';
+    float f = 10.1;
+    double d = 11.1;
+
+    printf("i = %d a = %c f = %f d = %lf \n", i, a, f, d);
+
+    return 0;
+}
