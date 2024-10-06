dbde7dc134865344f9d366e7bd034f5de71e4d33
dejeong <ahjeong921@gmail.com>
2024-10-06 23:36:23 +0900

    예제3 풀이

---
+++ programming-basic/week06/15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct data{
+    int month;
+    int day;
+};
+
+int main(){
+    struct data test = {12, 25};
+    struct data *ptr;
+    ptr = &test;
+
+    printf("month = %d, day = %d \n", test.month, test.day);
+    printf("month = %d, day = %d \n", (*ptr).month, (*ptr).day);
+    printf("month = %d, day = %d \n", ptr -> month, ptr -> day);
+
+    return 0;
+}
